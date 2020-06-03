a501b65683601e954ca73eab216076cfac0b0426
guicgs <etohlif@protonmail.com>
2020-06-03 19:36:00 -0300

    outras formas de fazer o 19 (count com parâmetro)

--- desafio19.js
+++ desafio19.js
@@ -4,3 +4,7 @@ db.produtos.find({ nome: { $regex: /Mc/i } }).count();
 
 // outra forma
 db.produtos.find({ nome: /Mc/i }).count();
+
+// outra forma
+db.produtos.count({ nome: { $regex: /Mc/i } });
+db.produtos.count({ nome: /Mc/i });
