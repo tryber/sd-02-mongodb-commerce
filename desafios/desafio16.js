06aa68477275612fc8543915aba7ff3c4fe474e2
eddyeboy27 <lipe0503@gmail.com>
2020-06-04 21:38:43 -0300

    Finalizado req16

--- desafios/desafio16.js
+++ desafios/desafio16.js
@@ -1,1 +1,13 @@
 use commerce;
+
+db.produtos.updateMany(
+  {},  
+  {
+    $push: {
+      "valoresNutricionais": {
+        $each: [], 
+        $sort: { "valoresNutricionais.percentual": -1 }
+      }
+    }
+  }
+);
